501c6425e0900846e5bf6342f7af0e508cd8772d
Lorgar-Avrelian <victor-14-244@mail.ru>
2024-02-07 09:29:37 +0300

    Project initialization

---
+++ src/main/java/test/task/crptapi/TestTask3Application.java
@@ -0,0 +1,13 @@
+package test.task.crptapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTask3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTask3Application.class, args);
+	}
+
+}
